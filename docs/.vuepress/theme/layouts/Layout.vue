46163e300663c1231c6bb49b4507fb79e4fcc6ee
Martin Jäger <martin@libre.solar>
2021-04-11 15:22:57 +0200

    Add GitHub and Twitter links

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -2,7 +2,10 @@
   <Layout>
     <template #page-bottom>
       <div class="footer">
-        Content provided by The Libre Solar Project under <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode">CC-BY-SA 4.0 License</a> | <a href="https://libre.solar/about/contact.html">Contact / Impressum</a>
+        <span style="padding: 5px;"><a href="https://twitter.com/LibreSolar"><img src="/images/twitter-logo.svg" height="14" /> Twitter</a></span>
+        <span style="padding: 5px;"><a href="https://github.com/LibreSolar"><img src="/images/github-logo.svg" height="14" /> GitHub</a></span>
+        | <span style="padding: 5px;">Content provided by The Libre Solar Project under <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode">CC-BY-SA 4.0 License</a></span>
+        | <span style="padding: 5px;"><a href="https://libre.solar/about/contact.html">Contact / Impressum</a></span>
       </div>
     </template>
   </Layout>
